935df6c9e1d6064a14aa6c352144ba043516dd55
K-Keita <k.syarin610@gmail.com>
2023-05-12 09:44:38 +0900

    install mantine

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,28 +1,38 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Button, MantineProvider } from "@mantine/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="text-3xl font-bold underline text-blue-500">
-          Hello world!
-        </h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="text-3xl font-bold underline text-blue-500">
+            Hello world!
+          </h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Button
+            className="ml-3 disabled:bg-opacity-30 disabled:bg-blue-300 bg-red-500"
+            // onClick={() => submitQuestion(message)}
+            // disabled={isLoading}
+          >
+            送信
+          </Button>
+        </header>
+      </div>
+    </MantineProvider>
   );
 }
 
