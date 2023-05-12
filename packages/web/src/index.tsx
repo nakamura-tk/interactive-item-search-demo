da934b6b9f4f272f8740c8222fd28a0ab2ed9bff
K-Keita <k.syarin610@gmail.com>
2023-05-12 09:52:33 +0900

    install msw

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { rest, setupWorker } from "msw";
+import reportWebVitals from "./reportWebVitals";
+import urlJoin from "url-join";
+import { url } from "./components/common";
+import { AnswerMessage, QuestionMessage } from "./types";
+
+const worker = setupWorker(
+  rest.post<QuestionMessage>(
+    urlJoin(url, "/chat-messages/:session_id"),
+    async (req, res, ctx) => {
+      const { message } = await req.json();
+
+      return res(
+        ctx.json({
+          items: [
+            {
+              description: "string",
+              id: "string",
+              image_url: "http://localhost:3000/images/sampleA.jpg",
+            },
+            {
+              description: "string",
+              id: "string",
+              image_url: "http://localhost:3000/images/sampleB.jpg",
+            },
+          ],
+          message: `answer:${message}${message}${message}${message}${message}${message}`,
+        } as AnswerMessage),
+        ctx.delay(2000)
+      );
+    }
+  )
+);
+
+worker.start();
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
