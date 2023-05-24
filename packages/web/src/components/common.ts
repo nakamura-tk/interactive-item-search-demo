d6343a1aa58314073e42b2188a05534a084cc4f2
Shuya Kinjo <a8109058@gmail.com>
2023-05-24 21:47:28 +0900

    APIエンドポイントを.envファイルに設定

--- packages/web/src/components/common.ts
+++ packages/web/src/components/common.ts
@@ -1,5 +1,5 @@
-export const url = process.env.API_URL
-  ? process.env.API_URL
+export const url = process.env.REACT_APP_API_URL
+  ? process.env.REACT_APP_API_URL
   : "http://127.0.0.1:8000";
 
 export const COOKIE_NAME = "interactive-item-search-item-cookie";
