38271e4fc168d7736e6c46999d033211aeb3c930
takahiro-nakamura <nakamura.takahiro@classmethod.jp>
2023-05-25 22:45:15 +0900

    ログ追加、OSS詳細情報取得処理追加

--- packages/server/api/services/hello.py
+++ packages/server/api/services/hello.py
@@ -1,5 +1,18 @@
+import os
 from api.models.hello import HelloResponse
+from boto3.session import Session
+from opensearchpy import OpenSearch, RequestsHttpConnection, AWSV4SignerAuth
 
 
 def hello() -> HelloResponse:
-    return {"message": "Hello!!!!"}
+    credentials = Session().get_credentials()
+    auth = AWSV4SignerAuth(credentials, "ap-northeast-1")
+    client = OpenSearch(
+        hosts=[{"host": os.environ["OPENSEARCH_ENDPOINT"], "port": 9200}],
+        http_compress=True,
+        use_ssl=False,
+        http_auth=auth,
+        connection_class=RequestsHttpConnection,
+    )
+    indices = client.cat.indices()
+    return {f'message": "Hello!!!!, indices: {indices}'}
